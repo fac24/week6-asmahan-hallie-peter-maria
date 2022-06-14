aa315a5f6438d68d0eb2e06664b28c06d91d383e
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-06-14 15:35:06 +0100

    Return query

--- database/model.js
+++ database/model.js
@@ -3,8 +3,8 @@ import db from "./connection"
 export function getCategories() {
 
     const get_categories = `SELECT plant_kind FROM plant_type`;
-    return db.query(get_categories).then((data) => { console.log(data.rows) });
-    //console.log(data.rows);
-    //const catagory = category.rows
-    //return catagory
+    return db.query(get_categories).then((data) => { return data.rows });
+  
 }
+
+
