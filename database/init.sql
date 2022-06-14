ef5761471273dfb997e0a3854346d9698ff896bd
Maria <maria.louisa.castro@gmail.com>
2022-06-14 11:25:19 +0100

    Begin setup

---
+++ database/init.sql
@@ -0,0 +1,46 @@
+BEGIN;
+
+DROP TABLE IF EXISTS plants, plant_type CASCADE;
+
+CREATE TABLE plant_type(
+    id SERIAL PRIMARY KEY,
+    plant_kind VARCHAR(255)
+);
+
+CREATE TABLE plants (
+    id SERIAL PRIMARY KEY,
+    plant_name VARCHAR(255),
+    plant_kind INTEGER REFERENCES plant_type(id),
+    price INTEGER
+);
+
+INSERT INTO plant_type (plant_kind) VALUES 
+('succulents'),
+('herbs'),
+('vegetables'),
+('garden plants');
+
+
+INSERT INTO plants (plant_name, plant_kind, price) VALUES 
+    ('Jade tree', 1, 5),
+    ('Aloe', 1, 5),
+    ('Yuca', 1, 5),
+    ('Wax Agave', 1, 5),
+    ('Paddle', 1, 5),
+    ('Parsely', 2, 5),
+    ('Basil', 2, 5),
+    ('Thyme', 2, 5),
+    ('Rosemary', 2, 5),
+    ('Dill', 2, 5),
+    ('Tomatoes', 3, 5),
+    ('Peppers', 3, 5),
+    ('Carrots', 3, 5),
+    ('Courgette', 3, 5),
+    ('Chilli', 3, 5),
+    ('Lavender', 4, 5),
+    ('Roses', 4, 5),
+    ('Peony', 4, 5),
+    ('Sunflower', 4, 5),
+    ('Daisy', 4, 5);
+
+COMMIT;
