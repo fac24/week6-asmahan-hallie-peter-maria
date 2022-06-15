985ad3ced25b78fc37f790f3c027dfb44482cd8f
Asmahan <asmahanhmohamed@gmail.com>
2022-06-15 11:47:52 +0100

    categories template

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
+import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import {getCategories} from '../database/model.js'
+import { getCategories } from "../database/model.js";
 
 export async function getStaticProps() {
   const categories = await getCategories();
@@ -17,6 +18,25 @@ export default function Home({ categories }) {
   return (
     <div>
       <h1>Little Garden</h1>
+      <section className="categories">
+        <ul>
+          {categories.length ? (
+            categories.map((category) => (
+              <Link href={`/categories/${category.id}`} key={category.id}>
+                <a>
+                  <li>
+                    <div>
+                      <h2>{category.plant_kind}</h2>
+                    </div>
+                  </li>
+                </a>
+              </Link>
+            ))
+          ) : (
+            <li>No results found</li>
+          )}
+        </ul>
+      </section>
     </div>
   );
 }
---
+++ pages/plants/herbs.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function Herbs() {
+  return (
+    <>
+      <h1>Choose you herb</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
+}
