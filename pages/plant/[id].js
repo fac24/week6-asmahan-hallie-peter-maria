a54842353ac1c1d6de68d291023493ecbb823c94
Maria <maria.louisa.castro@gmail.com>
2022-06-16 18:31:53 +0100

    Add form for for purchase options on dynamic item page

--- pages/plant/[id].js
+++ pages/plant/[id].js
@@ -1,5 +1,4 @@
 import Layout from "../../components/layout";
-import Link from "next/link";
 import Image from "next/image";
 // getting categories of plants
 import { getAllIds, getPlant } from "../../database/model.js";
@@ -34,36 +33,56 @@ export default function Plant({plantData}) {
     console.log(plantData)
   return (
     <Layout home >
-        <div>hi plant</div>
-        {/* <div>
+         <div>
       <section className="plants">
         <ul>
-          {categoryData ? (
-            categoryData.map((plant) => (
-              <Link href={`/plant/${plant.id}`} key={plant.id}>
-                <a>
-                  <li>
+          {plantData ? (
+              <>
                     <div>
+                        <h2>{plantData[0].plant_name}</h2>
                     <Image
                         //  className={styles.singleImage}
-                         src={plant.img_url}
+                         src={plantData[0].img_url}
                          height={200}
                          width={250}
-                         alt={plant.plant_name}
+                         alt={plantData[0].plant_name}
                     />
-                      <h2>{plant.plant_name}</h2>
-                      <p>From £{plant.price}</p>
+                      <p>{plantData[0].plant_description}</p>
                     </div>
-                  </li>
-                </a>
-              </Link>
-            ))
+                    <form
+                     //onSubmit={handleSubmit} action="/api/basket"
+                     >
+                    <lable htmlFor="size">Select your size:</lable>
+                    <select 
+                    id="name"
+                    name="name"
+                    //We will need a use state to implement functinality
+                    //onChange={(e) => setSize(e.target.value)}
+                    >
+                    <option value="seedling"> Seedling </option>
+                     <option value="small"> Small </option>
+                      <option value="medium"> Medium </option>
+                    </select>
+                    <br></br>
+                    <label htmlFor="quantity"> Quantity </label>
+                    <input
+                    type="number"
+                    min="1"
+                     max="10"
+                    name="quantity"
+                    id="quantity"
+                    //onChange={(e) => setQuantity(e.target.value)}
+                     ></input>
+                     <button type="submit">Add to basket</button>
+                    </form>
+                    </>
+
           ) : (
             <li>No results found</li>
           )}
         </ul>
       </section>
-    </div> */}
+    </div> 
     </Layout >
   );
 }
