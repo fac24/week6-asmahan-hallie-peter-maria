04862a0b9104d97bf078dce06cc4b03e41f8acc8
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-06-17 00:05:36 +0100

    made logo larger on page width and height

--- components/layout.js
+++ components/layout.js
@@ -14,8 +14,8 @@ export default function Layout({ children, home }) {
             <Image
             className={styles.container}
             src="/Little-Garden.png"
-            height={250}
-            width={250}
+            height={350}
+            width={350}
             alt="logo"
           />
          <Head>
